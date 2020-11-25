85a76ab0a7c46f5480a359cdc6d5c4e3ed76226e
LeeTrent <Lee.Trent.1@gmail.com>
2020-11-25 11:47:47 -0600

    Converts App from a functional component to a class component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
- return (
-    <div className="ui container" style={ { marginTop: '10px' } } >
-        <SearchBar />
-    </div>
-);   
+class App extends React.Component {
+    
+    onSeachSubmit(term) {
+        console.log("[App][onSearchSubmit] =>", term);
+    }
+    
+    render() {
+        return (
+            <div className="ui container" style={ { marginTop: '10px' } } >
+                <SearchBar onSubmit={this.onSeachSubmit} />
+            </div>            
+        );
+    }
 }
-
 export default App;
