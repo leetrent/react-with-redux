8dbab311235042e4b2d14b909c86b9308d5a7cde
LeeTrent <Lee.Trent.1@gmail.com>
2020-11-25 21:19:46 -0600

    Uses axios to fetch photos

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     
-    onSeachSubmit(term) {
-        console.log("[App][onSearchSubmit] =>", term);
+    ////////////////////////////////////////////////////////////////////////////////////
+    // USING PROMISE
+    ////////////////////////////////////////////////////////////////////////////////////
+    // onSeachSubmit(term) {
+    //     axios.get('https://api.unsplash.com/search/photos', {
+    //         params: {query: term},
+    //         headers: {
+    //             Authorization: 'Client-ID ',
+    //         }
+    //     })
+    //     .then(response => {
+    //         console.log(response.data.results);
+    //     });
+    // }
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // USING ASYNC / AWAIT SYNTAX
+    ////////////////////////////////////////////////////////////////////////////////////    
+    async onSeachSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID ',
+            }
+        });
+        console.log(response.data.results);
     }
-    
+    ////////////////////////////////////////////////////////////////////////////////////
+
     render() {
         return (
             <div className="ui container" style={ { marginTop: '10px' } } >
