b258014f1f4b349258c5dba19adfee6689214995
LeeTrent <Lee.Trent.1@gmail.com>
2020-11-27 13:43:52 -0600

    Renders list of images to page

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID ',
+        Authorization: 'Client-ID Q8GQ2mi1Wl3yXf6tJFCHKCa6IaodjRoTwywqjEm5kv0',
     }    
 });
