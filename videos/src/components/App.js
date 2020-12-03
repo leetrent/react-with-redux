765319a03a408936b512d6f8c2a650fd5aa0811b
LeeTrent <lee.trent.1@gmail.com>
2020-12-03 07:24:07 -0600

    Passes callback function to child components

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from  './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) => {
         console.log("[App][onTermSubmit] => (term)", term);
@@ -19,11 +19,15 @@ class App extends React.Component {
         console.log("[App][onTermSubmit] => (response.data.items)", response.data.items);
     };
 
+    onVideoSelect = (video) => {
+        console.log("[App][onVideoSelect] => (video): ", video);
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}  />
             </div>
         );
     }
