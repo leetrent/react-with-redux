8573e48718a88470407dfd78965e8c630278ebd9
LeeTrent <Lee.Trent.1@gmail.com>
2020-12-07 20:35:25 -0600

    Adds 'debouncedTerm' to state

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 // import Accordion from './components/Accordion';
 import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is front-end Javascript library.'
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite Javascript library among fron-end developers.'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React to create Javascript components.'
-  }
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is front-end Javascript library.'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite Javascript library among fron-end developers.'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React to create Javascript components.'
+//   }
+// ];
 
 export default () => {
   return (
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,41 +3,40 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect( () => {
-    const search = async() => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-        params: {
-          action: 'query',
-          list: 'search',
-          origin: '*',
-          format: 'json',
-          srsearch: term,
-        },
-      });
-      setResults(data.query.search);
-    };
+    const timerId = setTimeout( () => {
+      setDebouncedTerm(term);  
+    }, 1000);
 
-		if ( term && !results.length ) {
-			search();
-		} else {
-			const timeoutId = setTimeout( () => {
-				if (term) {
-					search();
-				}
-			}, 1000);
-			return () => {
-				clearTimeout(timeoutId);
-			};			
-		}
-  }, [term] );
+    return () => {
+      clearTimeout(timerId);  
+    };
+  }, [term]);
+  
+  useEffect( () => {
+    const search = async() => {
+        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: debouncedTerm,
+          },
+        });
+        setResults(data.query.search);
+      };
+      search();
+  }, [debouncedTerm] );
 
   const renderedResults = results.map( (result) => {
       return (
           <div key={result.pageid} className="item">
             <div className="right floated content">
-              <a className="ui button" target="_blank" 
+              <a className="ui button" target="_blank" rel="noreferrer"
                   href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
             </div>
             <div className="content">
