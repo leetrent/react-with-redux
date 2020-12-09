70860962752b6cdcd5dd35f50474651250336584
LeeTrent <lee.trent.1@gmail.com>
2020-12-08 20:50:44 -0600

    Working on closing dropdown when dropdown looses focus

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+
+    // useEffect( () => {
+    //     document.body.addEventListener('click', () => {
+    //         setOpen(false);
+    //     });
+    // }, [] );
 
     const renderedOptions = options.map( (option) => {
         if ( option.value === selected.value ) {
@@ -14,6 +20,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
             </div>
         );
     });
+    
     return (
         <div className="ui form">
             <div className="field">
