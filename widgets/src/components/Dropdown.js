21a625f39c549e77b86a4b2cdb9af5420bfa41a4
LeeTrent <lee.trent.1@gmail.com>
2020-12-09 20:51:50 -0600

    Fixed event listener bug

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
 
     useEffect( () => {
-        document.body.addEventListener('click', () => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current && ref.current.contains(event.target)) {
+                return;
+            }
             setOpen(false);
         }, {capture: true});
     }, [] );
@@ -22,7 +26,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
     
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a color:</label>
                 <div onClick={ () => setOpen(!open) } 
