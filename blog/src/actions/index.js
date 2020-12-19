ea36988675a39951d614024ef86a142a5d9f0e0d
LeeTrent <lee.trent.1@gmail.com>
2020-12-18 20:33:44 -0600

    Adds redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,8 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get('/posts');
-    dispatch( {type: 'FETCH_POSTS', payload: response} );
-  };
+// export const fetchPosts = () => {
+//   return async (dispatch) => {
+//     const response = await jsonPlaceholder.get('/posts');
+//     dispatch( {type: 'FETCH_POSTS', payload: response} );
+//   };
+// };
+
+// EQUIVALENT SYNTAX
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch( {type: 'FETCH_POSTS', payload: response} );
 };
