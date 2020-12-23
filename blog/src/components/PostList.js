24b54529ca980ee60e72a744c90eaea9d9f74d33
LeeTrent <lee.trent.1@gmail.com>
2020-12-23 10:04:10 -0600

    Adds method 'fetchPostsAndUsers'

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -29,8 +29,7 @@ class PostList extends React.Component {
   render() {
     return (
       <div className="ui relaxed divided list">
-      {this.
-      renderList()}
+      {this.renderList()}
       </div>
     );
   }
@@ -40,4 +39,4 @@ const mapStateToProps = (state) => {
   return { posts: state.posts };
 };
 
-export default connect( mapStateToProps, {fetchPosts} )(PostList);
+export default connect( mapStateToProps, {fetchPostsAndUsers} )(PostList);
