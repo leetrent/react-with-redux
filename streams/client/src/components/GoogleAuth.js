42662bf7a0c8cac0e9dfe518ae58ef0a96188e72
LeeTrent <lee.trent.1@gmail.com>
2021-01-01 07:47:08 -0600

    Comments code

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -7,7 +7,7 @@ class GoogleAuth extends React.Component {
   componentDidMount() {
    window.gapi.load('client:auth2', () => {
      window.gapi.client.init({
-       clientId: '135594557408-rusult4clens9d2elmuidm4ljg038g5f.apps.googleusercontent.com',
+       clientId: '',
        scope: 'email'
      })
      .then(() => {
